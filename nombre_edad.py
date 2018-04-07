5c305dc50a554d82b9f7a11c88984eba48629b51
gabbeehh <38160412+gabbeehh@users.noreply.github.com>
2018-04-07 13:37:02 -0500

    clase

---
+++ nombre_edad.py
@@ -0,0 +1,34 @@
+"""print ("inicio\n")
+nombre= "F. Gabriel C. Vazquez"
+edad=20
+
+print("Hola", nombre,"tu edad es:",edad)
+
+A = B = 666
+C ="Gabo"
+print("A, c")
+
+
+print ("Dime tu nombre\n")
+nombre=input()
+edad=24
+print("Hola", nombre, "¿Como estas?")
+
+sNombre="Gabriel"
+iEdad=20
+fAltura=1.75
+iPeso=80
+sFacultad= "F.E.S. Aragon"
+sCarrera= "Ing. en computacion"
+sSemestre= "segundo semestre"
+print("Alumno: ",sNombre, "\n", "Edad: ", iEdad, "Altura: ", fAltura, "Peso: ", iPeso, "\n", sFacultad,",", sCarrera, ",", sSemestre)
+"""
+
+str(sNombre=input("\nIngrese su nombre:")
+int (iEdad= input("\nIngresa tu edad:"))
+float (fAltura= input("\nIngresa tu estatura:"))
+float (fPeso=input("\nIngresa tu peso:"))
+str(sFacultad=input("\nIngresa tu escuela:")
+str (sCarrera=input("\nIngresa tu carrera:"))
+str(sSemestre=input("\nIngresa tu semestre:")))
+print("Alumno: ",sNombre, "\n", "Edad: ", iEdad, "Altura: ", fAltura, "Peso: ", fPeso, "\n", sFacultad,",", sCarrera, ",", sSemestre)
